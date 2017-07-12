df9618d0d4cb365e7531c9dde5f7cb2fbea455d6
Oleg Dreyman <dreymonde@me.com>
2017-07-12 15:26:27 +0300

    Basic add notification content target; substitute storyboard

---
+++ Match Notification/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  Match Notification
+//
+//  Created by Олег on 12.07.17.
+//  Copyright © 2017 The Great Game. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
