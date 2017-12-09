d27ff2060fa86440b43e337f6ff0ad46445c18ca
Oleg Dreyman <dreymonde@me.com>
2017-12-09 22:04:40 +0200

    Using .default configuration instead of .ephemeral

--- TheGreatPlayground.playground/Contents.swift
+++ TheGreatPlayground.playground/Contents.swift
@@ -3,11 +3,4 @@
 import UIKit
 import TheGreatKit
 
-let session = URLSession.init(configuration: .ephemeral)
-    .asReadOnlyCache()
-    .usingURLKeys()
-    .droppingResponse()
-let repo = GitHubRawFilesRepo(owner: "dreymonde", repo: "thegreatgame-storage", networkCache: session).asReadOnlyCache()
-let matchesAPI = MatchesAPI(dataProvider: repo).all.makeSyncCache()
-let matches = try matchesAPI.retrieve()
-print(matches)
+let newGitAPI = API.gitHubRaw()
